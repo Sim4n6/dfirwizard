4deb99c4a3ed79125d14ada4610ff304d09931b6
Sim4n6 <sim4n6@gmail.com>
2019-03-04 19:38:49 +0100

    update to py3

--- dfirwizard-v3.py
+++ dfirwizard-v3.py
@@ -7,22 +7,22 @@ import sys
 import pytsk3
 import datetime
 
-imagefile =  "Input\ntfs-img-kw-1.dd" # "\\\\.\\PhysicalDrive0"
+imagefile = r"\\\\.\\PhysicalDrive0"
 imagehandle = pytsk3.Img_Info(imagefile)
 partitionTable = pytsk3.Volume_Info(imagehandle)
 for partition in partitionTable:
-  print(f"{partition.addr}, {partition.desc}, {partition.start}({partition.start * 512}), {partition.len}")
-  if 'NTFS' in partition.desc:
-    filesystemObject = pytsk3.FS_Info(imagehandle, offset=(partition.start*512))
-    fileobject = filesystemObject.open("/$MFT")
-    print("File Inode: {fileobject.info.meta.addr}")
-    print("File Name: {fileobject.info.name.name}")
-    print("File Creation Time: {datetime.datetime.fromtimestamp(fileobject.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S')}")
-    
-    outFileName = str(partition.addr)+fileobject.info.name.name
-    print(outFileName)
-    
-    outfile = open(outFileName, 'w')
-    filedata = fileobject.read_random(0,fileobject.info.meta.size)
-    outfile.write(filedata)
-    outfile.close
+	print(f"{partition.addr}, {partition.desc}, {partition.start}({partition.start * 512}), {partition.len}")
+	if 'NTFS' in partition.desc:
+		filesystemObject = pytsk3.FS_Info(imagehandle, offset=(partition.start*512))
+		fileobject = filesystemObject.open("/$MFT")
+		print(f"File Inode: {fileobject.info.meta.addr}")
+		print(f"File Name: {fileobject.info.name.name}")
+		print(f"File Creation Time: {datetime.datetime.fromtimestamp(fileobject.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S')}")
+
+		outFileName = str(partition.addr)+fileobject.info.name.name
+		print(outFileName)
+
+		outfile = open(outFileName, 'w')
+		filedata = fileobject.read_random(0,fileobject.info.meta.size)
+		outfile.write(filedata)
+		outfile.close
