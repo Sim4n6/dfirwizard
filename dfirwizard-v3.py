0e975bda51fd3dc8c6b964ab86e7c2d995ffd02d
Sim4n6 <sim4n6@gmail.com>
2019-03-04 20:27:28 +0100

    update to py3

--- dfirwizard-v3.py
+++ dfirwizard-v3.py
@@ -3,26 +3,25 @@
 # Sample program or step 3 in becoming a DFIR Wizard!
 # No license as this code is simple and free!
 
-import sys
 import pytsk3
-import datetime
+from datetime import datetime as dt
 
-imagefile = r"\\\\.\\PhysicalDrive0"
+imagefile = r"/dev/sdb" #r"\\\\.\\PhysicalDrive0"
 imagehandle = pytsk3.Img_Info(imagefile)
 partitionTable = pytsk3.Volume_Info(imagehandle)
 for partition in partitionTable:
 	print(f"{partition.addr}, {partition.desc}, {partition.start}({partition.start * 512}), {partition.len}")
-	if 'NTFS' in partition.desc:
+	if b'NTFS' in partition.desc:
 		filesystemObject = pytsk3.FS_Info(imagehandle, offset=(partition.start*512))
 		fileobject = filesystemObject.open("/$MFT")
 		print(f"File Inode: {fileobject.info.meta.addr}")
 		print(f"File Name: {fileobject.info.name.name}")
-		print(f"File Creation Time: {datetime.datetime.fromtimestamp(fileobject.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S')}")
+		print(f"File Creation Time: {dt.fromtimestamp(fileobject.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S')}")
 
-		outFileName = str(partition.addr)+fileobject.info.name.name
+		outFileName = str(partition.addr)+str(fileobject.info.name.name)
 		print(outFileName)
 
-		outfile = open(outFileName, 'w')
-		filedata = fileobject.read_random(0,fileobject.info.meta.size)
+		outfile = open(outFileName, 'wb')
+		filedata = fileobject.read_random(0, fileobject.info.meta.size)
 		outfile.write(filedata)
 		outfile.close
